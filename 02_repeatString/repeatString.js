9c1aaea9cd02890d40b5da65564072406e9b68d2
Roshan <roshanpokhrel013@gmail.com>
2022-05-26 23:23:09 +0545

    exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = repeat('roshay',3);
+repeat(string, num)
+{
+    var times;
+    var word;
+    for(i=1;i<=times;i++)
+    {
+        var finalstring = repeatString + word;
+    }
+    return finalstring;
+}
+console.log(repeatString);
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function([]) {
+    var word = [n];
+    for(i=n;i<=0;i--)
+    {
+        reversed = console.log(word[i]);
+    }
+    return reversed;
+
 
 };
+ /*function reverseString(string)
+ {
+
+ }*/
 
 // Do not edit below this line
 module.exports = reverseString;
